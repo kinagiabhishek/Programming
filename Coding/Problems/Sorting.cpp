34f8058bba03ae5522394927d000da9d0a3fa13b
Abhishek Kinagi <kinagiabhishek@gmail.com>
2020-04-30 22:30:24 -0700

    Sorting Code Changes/ Initialization of VSCode

--- Coding/Problems/Sorting.cpp
+++ Coding/Problems/Sorting.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include<bits/stdc++.h>
 using namespace std;
-class Sort{
+class SortUtil{
 public:
     void print(vector<int> nums){
         for(int i:nums)
@@ -23,24 +23,24 @@ public:
     }
     void mergeSort(vector<int> nums){
     }
-    void quickSort(vector<int> nums,int l,int r){
+    void quickSort(vector<int>& nums,int l,int r){
         if(l<r){
             int p=partitionQ(nums,l,r);
             quickSort(nums,l,p-1);
             quickSort(nums,p+1,r);
         }
     }
-    int partitionQ(vector<int> nums,int l,int r){
+    int partitionQ(vector<int>& nums,int l,int r){
         int pivot=nums[r];
         int index=l-1;
         for(int i=l;i<r;i++){
             if(nums[i]<pivot){
                 index++;
-                swap(&nums[i],&nums[index]);
+                swap(nums[i],nums[index]);
             }
         }
-        swap(&nums[i+1],&nums[r]);
-        return i+1;
+        swap(nums[index+1],nums[r]);
+        return index+1;
     }
     void quickSort(vector<int> nums){
         quickSort(nums,0,nums.size()-1);
@@ -51,9 +51,18 @@ public:
 };
 int main()
 {
+    //Test Vector to be sorted
     vector<int> nums={5,2,1,4,3,7,9,6,8,10};
-    Sort s;
-    //s.bubbleSort(nums);
+    
+    //Initialize the sort util class object
+    SortUtil s;
+    
+    //Apply the sorting algorithms
+    cout<<"Bubble Sort"<<endl;
+    s.bubbleSort(nums);
+    
+    cout<<"Quick Sort"<<endl;
     s.quickSort(nums);
+    
     return 0;
 }
