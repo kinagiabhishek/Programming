2f70a2632092138ea43fa91ef1c966cc8c4514a4
Abhishek Kinagi <kinagiabhishek@gmail.com>
2020-05-04 00:55:38 -0700

    Modified Number Complement

--- Problems/LeetCode/May LeetCoding Challenges/Number Complement.cpp
+++ Problems/LeetCode/May LeetCoding Challenges/Number Complement.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     int findComplement(int num) {
-        vector<int> binaryRep;
-        int ans=0;
-        if(num==0)
-            return 1;
-        else{
-            while(num>0){
-                binaryRep.push_back((num%2)^1);
-                num/=2;
-            }
-        }
-        for(int i=0;i<binaryRep.size();++i)
-            ans+=(binaryRep[i]*pow(2,i));
-        return ans;
+        int bitmask=num;
+        bitmask|=(bitmask>>1);
+        bitmask|=(bitmask>>2);
+        bitmask|=(bitmask>>4);
+        bitmask|=(bitmask>>8);
+        bitmask|=(bitmask>>16);
+        return bitmask^num;
     }
 };
